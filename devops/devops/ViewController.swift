8593b5214d2e1ef16c030a345f99c7642bd122b7
chantungyee <1450126@tongji.edu.cn>
2019-03-26 20:04:33 +0800

    init

---
+++ devops/devops/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devops
+//
+//  Created by Dongyi on 26/03/2019.
+//  Copyright © 2019 Dongyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
